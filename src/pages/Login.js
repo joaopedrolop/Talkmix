e6e2f5d17f421d28d3d98e56aa69554ea9be78bf
HeloaGabriele <heloagabrielemari@gmail.com>
2025-08-13 15:38:15 -0300

    13/08/2025

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "react-native";
+import { Pressable, Text, TouchableOpacity, View, TextInput } from 'react-native';
+
+import { stylesLogin } from '../Styles/login';
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Login() {
+
+  const navigation = useNavigation();
+
+  return (
+
+  );
+}
